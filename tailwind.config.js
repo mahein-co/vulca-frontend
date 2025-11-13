67a864032d86c319d7eba0851a206603d73100f1
BenitoFezay <benitofezay@gmail.com>
2025-11-13 15:27:49 +0300

    Config colors.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#dc2626",
+        secondary: "#ef4444",
+        dark: "#1f2937",
+        grey: "#6b7280",
+        rose: "#db2777",
+      },
+    },
+  },
+  plugins: [],
+};

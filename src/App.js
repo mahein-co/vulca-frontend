3d90e44392747e32f521a2ebade452585259277a
BenitoFezay <benitofezay@gmail.com>
2025-11-13 15:32:03 +0300

    Mis en style de font family, configuration toast et store.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "@fontsource/roboto";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            style: {
+              duration: 5000,
+              fontSize: "0.85rem",
+              borderRadius: "8px",
+              padding: "12px 16px",
+              background: "#fff",
+            },
+            success: {
+              style: {
+                color: "#1f2937",
+              },
+            },
+            error: {
+              duration: 6000,
+              style: {
+                color: "#1f2937",
+              },
+            },
+          }}
+        />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 

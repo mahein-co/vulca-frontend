09675a2d34fc74e32863aa12e20a179fca25ea71
BenitoFezay <benitofezay@gmail.com>
2025-11-13 15:28:10 +0300

    Global constant.

---
+++ src/constants/globalConstants.js
@@ -0,0 +1,10 @@
+export const BASE_URL_API = process.env.REACT_APP_API_URL;
+
+export const PATHS = {
+  home: "/",
+  login: "/auth/login",
+  register: "/auth/register",
+  forgotPassword: "/auth/forgot-password",
+  dashboard: "/app",
+  ocr: "/app/ocr",
+};

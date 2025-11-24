e1d0cbd8bf66e73f56498d8b6ee03c933fe89f02
BenitoFezay <benitofezay@gmail.com>
2025-11-24 16:49:53 +0300

    Ajout de nouveaux routes.

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -1,6 +1,7 @@
 import IconDashboarb from "../assets/icons/dashboard.png";
 import IconOcr from "../assets/icons/pdf.png";
 import IconFinance from "../assets/icons/finance.png";
+import IconAdd from "../assets/icons/add.png";
 
 export const BASE_URL_API = process.env.REACT_APP_API_URL;
 
@@ -11,10 +12,15 @@ export const PATHS = {
   forgotPassword: "/auth/forgot-password",
   dashboard: "/app",
   ocr: "/app/ocr",
+  forms: "/app/forms",
+  facture: "/app/forms/facture",
+  achat: "/app/forms/achat",
+  banque: "/app/forms/bank",
 };
 
 export const SIDEBAR_NAVIGATIONS = [
   { title: "Dashboard", path: "/app", icon: IconDashboarb },
   { title: "Import file", path: "/app/ocr", icon: IconOcr },
   { title: "Finance", path: "/app/finance", icon: IconFinance },
+  { title: "Formulaire", path: "/app/forms", icon: IconAdd },
 ];

6e101a94b5edc64267a03632b6e6a768f312b17d
promes27 <franchescopromes@gmail.com>
2025-11-25 12:51:03 +0300

    scorecord

--- src/components/card/ScoreCard.jsx
+++ src/components/card/ScoreCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ScoreCard({ title, value, variation }) {
   return (
-    <div className="p-4 bg-white rounded-2xl shadow-md w-64 flex flex-col gap-2">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-2xl font-bold">{value}</p>
+    <div className="p-4 bg-white  rounded-2xl shadow-md w-64 flex flex-col gap-2">
+      <h3 className="text-lg font-semibold text-dark">{title}</h3>
+      <p className="text-2xl font-bold text-dark">{value}</p>
       <p className={`text-sm ${variation >= 0 ? "text-green-600" : "text-red-600"}`}>
         {variation >= 0 ? "+" : ""}{variation}%
       </p>

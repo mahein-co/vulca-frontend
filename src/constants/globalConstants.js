2e5977cbaa8ee8e52516a13b1d1d812387e85293
BenitoFezay <benitofezay@gmail.com>
2025-11-25 09:04:49 +0300

    Mis en ordre de navigation.

--- src/constants/globalConstants.js
+++ src/constants/globalConstants.js
@@ -20,7 +20,7 @@ export const PATHS = {
 
 export const SIDEBAR_NAVIGATIONS = [
   { title: "Dashboard", path: "/app", icon: IconDashboarb },
-  { title: "Import file", path: "/app/ocr", icon: IconOcr },
   { title: "Finance", path: "/app/finance", icon: IconFinance },
+  { title: "Import file", path: "/app/ocr", icon: IconOcr },
   { title: "Formulaire", path: "/app/forms", icon: IconAdd },
 ];
